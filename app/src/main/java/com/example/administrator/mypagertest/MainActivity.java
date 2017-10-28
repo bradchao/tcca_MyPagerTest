78c570dd8c37fd4304820b5bb96c66985cd68036
Brad Chao <brad@brad.tw>
2017-10-28 11:47:20 +0800

    201710281147

--- app/src/main/java/com/example/administrator/mypagertest/MainActivity.java
+++ app/src/main/java/com/example/administrator/mypagertest/MainActivity.java
@@ -14,10 +14,12 @@ import android.widget.FrameLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Page0 page0;
     private Page1 page1;
     private Page2 page2;
     private Page3 page3;
     private Page4 page4;
+    private Page5 page5;
     private FragmentManager fmgr;
     private FragmentTransaction tran;
 
@@ -40,12 +42,14 @@ public class MainActivity extends AppCompatActivity {
 
         mesg = (TextView)findViewById(R.id.mesg);
 
+        page0 = new Page0();
         page1 = new Page1();
         page2 = new Page2();
         page3 = new Page3();
         page4 = new Page4();
+        page5 = new Page5();
 
-        fragments = new Fragment[]{page1,page2,page3,page4};
+        fragments = new Fragment[]{page0,page1,page2,page3,page4,page5};
 
 
         fmgr = getSupportFragmentManager();
@@ -54,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
 //        tran.commit();
 
         viewPager.setAdapter(new MyPagerAdapter(fmgr));
-        viewPager.setCurrentItem(0);
+        viewPager.setCurrentItem(1);
 
 
     }
